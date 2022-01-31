9020b28205d0cc08f53d6f68e516fd78b9d075ef
AbilashPL <abhilash143.chintu@gmail.com>
2022-01-31 10:47:38 +0530

    First commit

---
+++ src/main/java/TestGroup/PracticeProblems2D/A25_TestNGAnnotationsA.java
@@ -0,0 +1,44 @@
++package TestGroup.PracticeProblems2D;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class A25_TestNGAnnotationsA {
+	 @BeforeSuite
+	  public void beforeSuite() {
+	    System.out.println("BeforeSuite");
+	  }
+
+	  @BeforeTest
+	  public void beforeTest() {
+	    System.out.println("BeforeTest");
+	  }
+
+	  @BeforeClass
+	  public void beforeClass() {
+	    System.out.println("BeforeClass");
+	  }
+
+	  @BeforeMethod
+	  public void beforeMethod() {
+	    System.out.println("BeforeMethod");
+	  }
+	  
+	  @Test
+	  public void myTestMethod1() {
+	    System.out.println("myTestMethod1");
+	  }
+
+	  @Test
+	  public void myTestMethod2() {
+	    System.out.println("myTestMethod2");
+	  }
+	  
+
+
+}
---
+++ src/main/java/TestGroup/PracticeProblems2D/Test.java
@@ -0,0 +1,11 @@
+package TestGroup.PracticeProblems2D;
+
+public class Test {
+
+	public static void main(String[] args) {
+		String a = "AbilashPL";
+		a = a.substring(a.indexOf('i'), a.indexOf('a'));
+		System.out.println(a);
+	}
+
+}
